7499cd73389ef47f355f5a41fb0dc3d896ca3393
kimlien0110 <kimlien01101994@gmail.com>
2021-07-22 09:47:30 +0200

    change folder component, page, service to lowercase

---
+++ event-planner/src/pages/Home.js
@@ -0,0 +1,11 @@
+import EventList from '../components/Events/EventList'
+
+const Home = () => {
+    return <section>
+            <div className="list-movie">
+                <EventList />
+            </div>
+    </section>
+}
+
+export default Home
---
+++ event-planner/src/services/Events.js
@@ -0,0 +1,23 @@
+export const getEvents = async () => {
+	let response = await fetch(`${process.env.REACT_APP_API_URL}/events/`, {
+		method: 'GET',
+		headers: {
+		  'Accept': 'application/json',
+		  'Content-Type': 'application/json'
+		}
+	})
+    let result = await response.json()
+    return result.events
+}
+
+export const getEventById = async (eventId) => {
+	let response = await fetch(`${process.env.REACT_APP_API_URL}/events/${eventId}`, {
+		method: 'GET',
+		headers: {
+		  'Accept': 'application/json',
+		  'Content-Type': 'application/json'
+		}
+	})
+    let result = await response.json()
+    return result
+}
